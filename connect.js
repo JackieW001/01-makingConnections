aa44c3f17ea2f1639aa18b45c986cdb9a363635c
JackieW001 <jwoo@stuy.edu>
2018-02-05 19:51:41 -0500

    done'

--- connect.js
+++ connect.js
@@ -1,44 +1,24 @@
-var print = function(e){
-    console.log("hi");
-}
 
 var canvas = document.getElementById('board');
 var ctx = canvas.getContext('2d');
-var pen = 0;
 
-var getMousePos = function(canvas, evt){
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
 
 var draw = function(e){
-    console.log(e);
-    console.log(e.x + ", " + e.y);
-    var pos = getMousePos(canvas, e);
-    posX = pos.x;
-    posY = pos.y;
 
- 
+    posX = e.offsetX;
+    posY = e.offsetY;
+
+    ctx.strokeStyle="black";
+    ctx.fillStyle="#FF0000";
+
+    var r  = Math.floor(Math.random()*50 + 10);
+    ctx.arc(posX, posY, r, 0, 2*Math.PI);
+    ctx.moveTo(posX,posY);
+
+    ctx.stroke();
+    ctx.fill();
+    
     ctx.lineTo(posX,posY);
-    // circle
-    if (pen == 0 ){
-	ctx.strokeStyle="#FF0000";
-	ctx.fillStyle="#FF0000";
-	ctx.arc(posX, posY, Math.floor(Math.random()*50 + 10), 0, 2*Math.PI);
-	ctx.moveTo(posX,posY);
-	ctx.stroke();
-	ctx.fill();
-    }
-    else{
-	ctx.strokeStyle="#0000FF";
-	var rectW = Math.floor(Math.random()*50) + 10;
-	var rectH = Math.floor(Math.random()*50) + 10;
-	ctx.strokeRect(posX-(rectW/2), posY-(rectH/2), rectW, rectH);
-    }
-
  }
 
 canvas.addEventListener('click', draw);
@@ -52,13 +32,4 @@ var clearCanvas = function(e){
 var clear = document.getElementById('clear');
 clear.addEventListener('click', clearCanvas);
 
-var togglePen = function(e){
-    if (pen == 0){
-	pen = 1;
-    }
-    else{
-	pen = 0;
-    }
-}
-var toggle = document.getElementById('toggle');
 toggle.addEventListener('click', togglePen);
